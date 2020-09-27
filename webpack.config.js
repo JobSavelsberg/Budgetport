f4593e4e7b7bd0c5b89f323ac8b056f4bd2b2e3e
Job Savelsberg <jobsavelsberg@gmail.com>
2020-09-27 23:26:16 +0200

    Remove uglify options

--- webpack.config.js
+++ webpack.config.js
@@ -100,13 +100,7 @@ if (process.env.NODE_ENV === 'production') {
         NODE_ENV: '"production"'
       }
     }),
-    new UglifyJsPlugin({
-      warnings: false,
-      ie8: false,
-      output: {
-        comments: false
-      }
-    }),
+    new UglifyJsPlugin(),
     new webpack.LoaderOptionsPlugin({
       minimize: true
     })

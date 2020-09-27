d1b5fc915f9cc51c9c4cfaf73bf8b9588d1c9d95
Job Savelsberg <jobsavelsberg@gmail.com>
2020-09-27 23:03:52 +0200

    Use .env instead of json for firebase auth

--- src/server/main.ts
+++ src/server/main.ts
@@ -13,24 +13,28 @@ import serveStatic from 'serve-static'
 import history from 'connect-history-api-fallback';
 import * as admin from 'firebase-admin';
 
-import firebaseAccountCredentials from './keys/serviceAccountKey.json'
+// initialize configuration
+dotenv.config();
+const credentials = {
+  type: "service_account",
+  project_id: process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key: process.env.FB_PRIVATE_KEY,
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FB_CLIENT_CERT
+}
 
-const serviceAccount = firebaseAccountCredentials as admin.ServiceAccount
+const serviceAccount = credentials as admin.ServiceAccount
 admin.initializeApp({
      credential: admin.credential.cert(serviceAccount),
      databaseURL: "https://budgetport-2644b.firebaseio.com"
 });
 
-/*admin.auth().verifyIdToken(idToken)
-  .then(function(decodedToken) {
-    let uid = decodedToken.uid;
-    // ...
-  }).catch(function(error) {
-    // Handle error
-  });*/
 
-// initialize configuration
-dotenv.config();
 const port = process.env.SERVER_PORT || 5000;
 
 const app = express();
